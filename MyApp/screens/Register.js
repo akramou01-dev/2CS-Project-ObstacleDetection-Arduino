7ed61ec0686f5701b4558dbe1f09a7943a2ff630
SouhilaGuendouzi <61289836+SouhilaGuendouzi@users.noreply.github.com>
2022-02-07 17:45:28 +0100

    Update Register.js

--- MyApp/screens/Register.js
+++ MyApp/screens/Register.js
@@ -1,51 +1,79 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@SouhilaGuendouzi 
+SouhilaGuendouzi
+/
+PluriProjectApp
+Private
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+PluriProjectApp/Register.js /
+@SouhilaGuendouzi
+SouhilaGuendouzi Create Register.js
+Latest commit 6b6f8aa 8 minutes ago
+ History
+ 1 contributor
+91 lines (73 sloc)  2.15 KB
+   
 import React ,{useState , useEffect} from 'react'
-import {Text, View, StyleSheet,LogBox} from 'react-native'
-import {Button, TextInput} from 'react-native-paper'
+import {Text, View,LogBox,TextInput ,Pressable} from 'react-native'
+import {Button, } from 'react-native-paper'
 import database from '@react-native-firebase/database';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { version } from '@babel/core';
+import registerStyle from '../Styles/registerStyle';
+import HomeAveugle from './HomeAveugle';
 
-export default function Register(props) {
+
+ const Register=(props)=> {
      
-    const [token,setToken]=useState("");
+    const [token,setToken]=useState();
 
     const storeData = async (value) => {
-        console.log("rah dkhal")
+  
         try {
           await AsyncStorage.setItem('@storage_Key', value)
          
         } catch (e) {
-          // saving error
+          console.log("souhila kayan erreur")
         }
       }
 
-      const getData = async () => {
+    const getData = async () => {
         try {
           const value = await AsyncStorage.getItem('@storage_Key')
           if(value !== null) {
-            props.navigation.navigate("Home",{data:value})
+            
+            props.navigation.navigate("HomeAveugle",{data:token})
           }
         } catch(e) {
           // error reading value
         }
       }
  
-    const styles=StyleSheet.create(
-        {
-            TextInput:{
-                padding:15,
-                marginTop:200
-               }
-        });
+   
 
-    const Verifier=()=>{
-        console.log(token)
+    const Verifier= ()=>{
+     
         database()
-  .ref(token+'/')
-  .on('value', snapshot => {
+     .ref(token+'/')
+     .on('value', snapshot => {
       if (snapshot.val()!=null) 
       {  storeData(token)
-          props.navigation.navigate("Home",{data:token})
+        return (
+          <HomeAveugle token={token}/>
+        )
         }
       else console.log("Token Error ")
   });
@@ -53,37 +81,54 @@ export default function Register(props) {
 
     useEffect(() => {
         getData();
-
           });
     
         
     return (
-     <View>
+     <View style={registerStyle.container}>
          {LogBox.ignoreLogs(['Setting a timer'])}
-        
+        <Text style={registerStyle.description}>
+        Please enter the code located in your electric arm
+        </Text>
          <TextInput
-         style={styles.TextInput}
-         label="Token"
+         style={registerStyle.input}
+         placeholder="Enter Token"
          value={token}
          mode="outlined"
          onChangeText={text=>
             {
                 setToken(text)
-                Verifier()
+               
             }
             }
-         >
+        />
 
-         </TextInput>
-         <Button
+         
+         <Pressable style={registerStyle.sub}
          onPress={()=>
-            {
+            {  console.log('hawala token',token)
                Verifier();
             }}
          >
-             Inscrir
-         </Button>
+            <Text style={registerStyle.subText}> 
+              Submit
+            </Text>
+         </Pressable>
      </View>
     )
    }
+   export default Register;
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Loading complete
 
